f16032ee37bb8c98de8af698afee886d1bb6e176
newmohib <@@mohib100@@>
2024-12-06 13:01:00 +0600

    added last fetched column in feeds

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -8,3 +8,11 @@ RETURNING *;
 SELECT * FROM feeds;
 
 
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds ORDER BY last_fetched_at DESC NULLS FIRST LIMIT 1;
+
+-- name: MarkFeedAsFetched :one
+UPDATE feeds
+SET last_fetched_at = NOW(), updated_at = NOW()
+WHERE id = $1
+RETURNING *;
